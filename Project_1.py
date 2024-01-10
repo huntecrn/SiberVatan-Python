194451ec6f453a0693079fe04e40f98f0fd76cf9
huntecrn <112987913+huntecrn@users.noreply.github.com>
2024-01-10 19:09:11 +0300

    First Commit
    This is my first commit

---
+++ Project_1.py
@@ -0,0 +1,23 @@
+#print ("hello my bad world")
+#print ("LALO")
+
+#kelime = "merhaba\n dunya" 
+#print (kelime)
+
+#sayi = -2
+#print("mutlak deger",abs(sayi))cls
+
+#us = pow(2,4)
+#print("sayi ussu",us)
+
+from math import sqrt,ceil,floor
+#sayi = 25
+#karekok= sqrt(sayi)
+#print("sayinin karekoku", karekok)
+
+#ondalik=10.25
+#yukari= ceil(ondalik)
+#asagi= floor(ondalik)
+#print("yukari",yukari)
+#print("asagi",asagi)
+
