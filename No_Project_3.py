e2ed6518624ced76feeccb75e4e3a32b813ce1cf
huntecrn <112987913+huntecrn@users.noreply.github.com>
2024-01-17 19:11:32 +0300

    Delete No_Project_3.py

--- No_Project_3.py
+++
@@ -1,7 +0,0 @@
-#kullanici_ad = input("Lütfen adınızı giriniz : ")
-#print("Hoşgeldiniz",kullanici_ad)
-
-#deger1 = int(input("Lütfen birinci değeri giriniz : "))
-#deger2 = int(input("Lütfen ikinci değeri giriniz : "))
-#print(deger1 + deger2)
-
