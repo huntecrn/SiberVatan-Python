e8d07f1e37d0147c10de175162942a36fedb2390
huntecrn <112987913+huntecrn@users.noreply.github.com>
2024-01-20 16:39:14 +0300

    Add files via upload

---
+++ My Projects/No_Project_4.py
@@ -0,0 +1,9 @@
+#ad = "enes"
+#soyad = "kose"
+#yas = 16
+#print("Benim adım",ad,soyad,"Yaşım",yas)
+
+ad = input("Lütfen adınızı giriniz")
+soyad = input("Lütfen soy adınızı giriniz")
+yas = input("Lütfen yaşınızı giriniz")
+print("Hoşgeldiniz",yas,"yaşındaki",ad,soyad)
---
+++ My Projects/No_Project_5.py
@@ -0,0 +1,76 @@
+# liste = [1,2,3,4,5]
+# print(liste)
+# print(liste[4])
+# print(type(liste))
+
+# liste[3] = 100
+# print(liste[3])
+# print(liste)
+
+# alt_liste = liste[2:4]
+# print(alt_liste)
+
+# tuple = (1,2,3)                                            CTRL+K+C = seçtiklerinin başına # koyar, CTRL+K+U = seçtiklerinin başındaki # leri kaldırır.
+# print(tuple)
+# print(tuple[1])
+# tuple[1] = 16
+
+# kume = {1,2,3}
+# print(kume)
+# kume.add(4)
+# print(kume)
+# kume.update([5,6])
+# print(kume)
+
+# dict = {'anahtar1':'deger1','anahtar2':'deger2'}
+# print(dict)
+# deger = dict['anahtar1']
+# print(deger)
+# kume_liste = list(kume)
+# print(kume_liste)
+# print(type(kume_liste))
+
+# list_keys = list(dict.keys())
+# list_values = list(dict.values())
+# print("keys",list_keys)
+# print("values",list_values)
+
+# list_keys = list(dict.keys())
+# print(type(list_keys))
+# print(list_keys)
+# list_values = list(dict.values())
+# print(type(list_keys))
+# print(list_values)
+
+sayilar = [1,2,3,4,5]
+# en_buyuk = max(sayilar)
+# en_kucuk = min(sayilar)
+# print(en_kucuk)
+# print(en_buyuk)
+# sayilar.append(6)
+# sayilar.append(7)
+
+# yeni_en_buyuk = max(sayilar)
+# yeni_en_kucuk = min(sayilar)
+# print(yeni_en_buyuk)
+# print(yeni_en_kucuk)
+# print(sayilar)
+
+#sayilar.pop()
+#print(sayilar)
+
+# sayilar.sort()
+# print(sayilar)
+
+# sayilar.reverse()
+# print(sayilar)
+
+#print(sayilar.count(1))
+
+# aralik = range(1,500)
+# print(list(aralik))
+
+import random
+
+# rastgele_sayi = random.randint(1,100)
+# print("tutulan sayı",rastgele_sayi)
